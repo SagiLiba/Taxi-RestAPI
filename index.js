14816bb633215c5e1494d9c6e0fdf4c883939a50
SagiLiba <libasagi@gmail.com>
2018-10-06 14:53:02 +0300

    Update index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+// Set up express app
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose'); // allows connection to mongodb
+const port = process.env.PORT || 3000;
+
+// Connect to monogodb - Creates it if it does not exists
+mongoose.connect('');
+mongoose.Promise = global.Promise; // Overriding mongoose promise because its deprecated
+
+app.use(express.static('public'));
+
+app.use(bodyParser.json());
+// Initialize routes
+app.use('/api',require('./routes/api'));
+// Error handling
+app.use(function(err,req,res,next){
+    res.status(422).send({error: err.message});
+});
+
+// Listen for requests
+app.listen(port,function(){
+    console.log('Listening for requests on port: ' + port);
+});
